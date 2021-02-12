24705365d94202bdc528167c581a8e0a58ff0ba0
Muhenned Abdulsahib <muhenned03@gmail.com>
2021-02-12 12:01:09 -0600

    update

--- Q_reg_plot2.R
+++ Q_reg_plot2.R
@@ -1,4 +1,5 @@
-# Plot of quantile regression for glucose reressed on Gender, Age,Race,BMI,Statin_status,BMI
+# Plot of quantile regression for BMI regressed on Gender, Age,Race,Statin_status
+#Using bsplines
 require(quantreg)
 library(tidyverse)
 library(splines)
@@ -20,11 +21,16 @@ bmi.data$Gender=recode(bmi.data$Gender, "1" = "Male", "2" = "Female" )
 # RIDRETH1 (race, 1=mexican, 2=otherHispanic, 3= Non-Hispanic White,4=Non-Hispanic Black ,5=Other Race - Including Multi-Racial
 
 bmi.data$Race=recode(bmi.data$Race, "1" = "Mexican", "2" = "Other_Hispanic","3"="White","4"="Black","5"="Other" )
-# Classifying population to diabetes and non diabetes
 
+
+# Save an object to a file
+saveRDS(bmi.data, file = "bmi.data.rds")
+# Restore the object
+bmi.data=readRDS(file = "bmi.data.rds")
+ 
  
 #formula <- Glu ~Gender+Age+Race+BMI+Statin_status+Age2+BMI2+Total_chol+Total_col2+Waist_cir 
-formula <- Glu ~Gender+Race+BMI+Statin_status+bs(Age,intercept=FALSE,df=5)+bs(Total_chol,intercept=FALSE,df=5)+bs(waist_cir,intercept=FALSE,df=5)      
+formula <- BMI ~Gender+Race+Statin_status+bs(Age,intercept=FALSE,df=5)+bs(Total_chol,intercept=FALSE,df=5)       
 fit3.ols <- summary(lm(formula,data =bmi.data))$coefficients
 #attach(bmi.data)
 p <- nrow(fit3.ols)
@@ -55,11 +61,11 @@ png(file = here::here("images", "newfig11.png"),
     res = 400, height = 9, width = 16, units = "in")
 p <- dim(fit3)[1]
 #blab <- c("Intercept","Male ","Age","RaceMexican", "RaceOther", "RaceOther_Hispanic ", "White ", "BMI", " Statin_status","Age Suqared","BMI^2","Total cholesterol","Total cholestrol^2","Waist Circumference")
-blab <- c("Intercept","Male ","Age","RaceMexican", "RaceOther", "RaceOther_Hispanic ", "White ", "BMI", " Statin_status","Age1","BMI2","Total cholesterol","Total cholestrol2","Waist Circumference",
-          "Age1","Age2","Age3","Age4","Age5","TC1","TC2","TC3","TC4","TC5","Wasist1","Wasist2","Wasist3","Wasist4","Wasist5")
-plot(summary(fit3))
+blab <- c("Intercept","Male ","RaceMexican", "RaceOther", "RaceOther_Hispanic ", "White ", " Statin_status" ,
+          "Age1","Age2","Age3","Age4","Age5","TC1","TC2","TC3","TC4","TC5")
+ 
 par(mfrow=c(6,4))
-for(i in c(1:23)){
+for(i in c(1:17)){
     #if(i==1){#adjust intercept to be centercept
      #   Age.bar <- mean(bmi.data$Age)
       #  BMI.bar <- mean(bmi.data$BMI)
@@ -94,22 +100,37 @@ for(i in c(1:23)){
     abline(h=bhat+qnorm(.95)*bhat.se,col=cols[6])
 }
 dev.off()
-#plot for marginal effect of weight gain for several gains
-#pdf("newfig2.pdf",width=7.0,height=7.0)
-png(file = here::here("images", "newfig22.png"),
+
+
+
+###########################
+#Or we can get this plot using quant regression summary plot.
+########################################################################
+
+
+
+
+png(file = here::here("images", "newfig1100.png"),
     res = 400, height = 9, width = 16, units = "in")
-par(mfrow=c(2,2))
-gains <- quantile(bmi.data$BMI,c(.1,.25,.75,.9))
-for(i in 1:length(gains)){
-    effect <- fit3[8,1,]+2*fit3[11,1,]*gains[i]
-    plot(taus,effect,xlab="Quantile",ylab="Weight Gain Effect")
-    lines(taus,effect)
-    title(paste("Weight Gain", format(round(gains[i])),"Lbs"))
-}
+par(mfrow=c(7,4))
+quantreg.all <- rq(formula, tau = seq(0.05, 0.95, by = 0.05), data=bmi.data)
+
+quantreg.plot <- summary(quantreg.all) 
+plot(quantreg.plot)
 dev.off()
-#Now try to plot quadratic effect of mother's age for several taus
-#pdf("newfig3.pdf",width=7.0,height=7.0)
-png(file = here::here("images", "newfig33.png"),
+
+
+
+
+
+
+
+
+
+ 
+#Now try to plot b splines effects of age on BMI for several taus
+ 
+png(file = here::here("images", "effect_Age_22.png"),
     res = 400, height = 9, width = 16, units = "in")
 # construct the b-spline expansion on the generated ages
 
@@ -123,204 +144,49 @@ ages_bs <- bs(ages, degree = attr(age_bs, "degree"), knots = attr(age_bs, "knots
               Boundary.knots = attr(age_bs, "Boundary.knots"), intercept = FALSE)
 
 for(i in c(6,9,15,22)){
-    effect <- fit3[9,1,i]*ages_bs[, 1] + fit3[10,1,i] * ages_bs[, 2] + fit3[11,1,i] * ages_bs[, 3] +
-        fit3[12,1,i] * ages_bs[, 4] + fit3[13,1,i] * ages_bs[, 5]
+    effect <- fit3[9,1,i]*ages_bs[, 1] + fit3[9,1,i] * ages_bs[, 2] + fit3[10,1,i] * ages_bs[, 3] +
+        fit3[11,1,i] * ages_bs[, 4] + fit3[12,1,i] * ages_bs[, 5]
     plot(ages,effect,type="n",xlab="Age",ylab="Age effect")
     lines(ages,effect)
-     title(paste("Age effects on glucose", format(round(taus[i],2)),"Quantile"))
+     title(paste("Age effects on BMI", format(round(taus[i],2)),"Quantile"))
 }
 dev.off()
 
-#Total cholestrol effects on glucose.
 
-png(file = here::here("images", "newfig44.png"),
+
+
+# Cholestrol effect on BMI
+
+png(file = here::here("images", "effect_TC_22.png"),
     res = 400, height = 9, width = 16, units = "in")
+#Total cholestrol effects on BMI.
+
+
 
 par(mfrow=c(2,2))
 # extract the splines from the model fit
-Total_Chol_bs <- bs(bmi.data$Total_chol,intercept=FALSE,df=5)
+Total_chol_bs <- bs(bmi.data$Total_chol,intercept=FALSE,df=5)
 Total_chol <- seq(min(bmi.data$Total_chol),300,by=1)
 
-Total_Chol_bs <- bs(Total_chol, degree = attr(Total_Chol_bs , "degree"), knots = attr(Total_Chol_bs, "knots"), 
-                  Boundary.knots = attr(Total_Chol_bs, "Boundary.knots"), intercept = FALSE)
-
+Total_chol_bs <- bs(Total_chol, degree = attr(Total_chol_bs , "degree"), knots = attr(Total_chol_bs, "knots"), 
+                    Boundary.knots = attr(Total_chol_bs, "Boundary.knots"), intercept = FALSE)
+QR.b <- boot.rq(cbind(1,Total_chol_bs),bmi.data$BMI,tau=0.5, R=10000)
 
 for(i in c(6,9,15,22)){
-    effect <- fit3[14,1,i]*Total_Chol_bs[, 1] + fit3[15,1,i] *Total_Chol_bs[, 2] + fit3[16,1,i] * Total_Chol_bs[, 3] + fit3[17,1,i] * Total_Chol_bs[, 4] +
-        fit3[18,1,i] * Total_Chol_bs[, 5]  
+    effect <- fit3[13,1,i]*Total_chol_bs[, 1] + fit3[14,1,i] *Total_chol_bs[, 2] + fit3[15,1,i] * Total_chol_bs[, 3] + fit3[16,1,i] * Total_chol_bs[, 4] +
+        fit3[17,1,i] * Total_chol_bs[, 5]  
     plot(Total_chol,effect,type="n",xlab="Total cholestrol",ylab="Total cholestrol effect")
     lines( Total_chol,effect)
-    title(paste("Total cholestrol effects on glucose", format(round(taus[i],2)),"Quantile"))
+    #for(j in seq_len(nrow(QR.b$B))) {
+    #    line(QR.b$B[j,1], QR.b$B[j,2] )
+    #}
+    title(paste("Total cholestrol effects on BMI", format(round(taus[i],2)),"Quantile"))
 }
 dev.off()
 
 
-#Waist circumference  effects on glucose.
 
-png(file = here::here("images", "newfig55.png"),
-    res = 400, height = 9, width = 16, units = "in")
-
-par(mfrow=c(2,2))
-# extract the splines from the model fit
-waist_cir_bs <- bs(bmi.data$waist_cir,intercept=FALSE,df=5)
-waist_cir <- seq(min(bmi.data$waist_cir),150,by=1)
-
-waist_cir_bs <- bs(waist_cir, degree = attr(waist_cir_bs , "degree"), knots = attr(waist_cir_bs, "knots"), 
-                    Boundary.knots = attr(waist_cir_bs, "Boundary.knots"), intercept = FALSE)
-
-
-for(i in c(6,9,15,22)){
-    effect <- fit3[19,1,i]* waist_cir_bs[, 1] + fit3[20,1,i] *waist_cir_bs[, 2] + fit3[21,1,i] * waist_cir_bs[, 3] + fit3[22,1,i] * waist_cir_bs[, 4] +
-        fit3[23,1,i] * waist_cir_bs[, 5]  
-    plot(waist_cir,effect,type="n",xlab="Total cholestrol",ylab="Total cholestrol effect")
-    lines( waist_cir,effect)
-    title(paste("Waist circumference effects on glucose", format(round(taus[i],2)),"Quantile"))
-}
-dev.off()
-
+#####################################
  
 
 
-
-bmi.data %>% subset(Gender == 1) %>% count(RXDDRUG) %>% arrange(desc(n))
-# Count number of male 5990 
-bmi.data %>% filter(Gender == 'Male')%>%count() 
-
-# Count number of female 6416 
-bmi.data %>% filter(Gender == 'Female')%>%count()
-
-# Count number of male  on statin 1185
-l=bmi.data %>% filter(Gender == 'Male')  
-l=count(l[l$Statin_status==1,]) 
-# Count number of Female on statin 1162 
-l=bmi.data %>% filter(Gender == 'Female')  
-l=count(l[l$Statin_status==1,]) 
-# Mean Age for Male 49.9
-bmi.data %>% filter(Gender == 'Male')%>% mean(Age)
-#error
-l=bmi.data[bmi.data$Gender=='Male',] 
-mean(l$Age)
-# Mean Age for Female 49.73
-l=bmi.data[bmi.data$Gender=='Female',] 
-mean(l$Age)
-# Mean glucose for Male  112.618
-l=bmi.data[bmi.data$Gender=='Male',] 
-mean(l$Glu)
-# Mean glucose for Female 106.47
-l=bmi.data[bmi.data$Gender=='Female',] 
-mean(l$Glu)
-
-########
-
- 
-######################################################
- 
-#######
- 
-
-###############
-
-library(ggeffects)
-library(splines)
-data(efc)
-fit1 <- lm(barthtot ~ c12hour + bs(neg_c_7) * c161sex + e42dep, data = efc)
-
-fit2=lm( BMI ~ X - 1, data=bmi.data, tau = 0.9)
- 
-ggpredict(fit1, terms = "Age")
-
-#############################################
-################################################################
-##############################################
-
-# extract the splines from the model fit
-age_bs <- bs(bmi.data$Age,intercept=FALSE,df=5)
-# generate a sequence of ages
-ages <- seq(min(bmi.data$Age),max(bmi.data$Age),by=1)
-# construct the b-spline expansion on the generated ages
-ages_bs <- bs(ages, degree = attr(age_bs, "degree"), knots = attr(age_bs, "knots"), Boundary.knots = attr(age_bs, "Boundary.knots"), intercept = FALSE)
-
-for(i in c(6,9,15,22)){
-    # evaluate the spline basis times the coefficients
-    effect <- fit3[9,1,i]*ages_bs[, 1] + fit3[10,1,i] * ages_bs[, 2] + fit3[11,1,i] * ages_bs[, 3] + fit3[12,1,i] * ages_bs[, 4] + fit3[13,1,i] * ages_bs[, 5]
-    plot(ages,effect,type="n",xlab="Age",ylab="Age effect")
-    lines(ages,effect)
-    title(paste("Age effects on glucose", format(round(taus[i],2)),"Quantile"))
-}
-
-# Another form for effect calculation
-#effect <- ages_bs %*% fit3[9:13, 1, i]
-
-########################################################
-##################################################################################
-########################################################
-
- library(splines)
-
- plot(bmi.data$Age,bmi.data$Total_chol,xlab = "milliseconds", ylab = "acceleration",type="n")
- points(bmi.data$Age,bmi.data$Total_chol,cex = .75)
- X <- model.matrix(bmi.data$Glu ~ bs(bmi.data$Age, df=15)+bs(bmi.data$Total_chol,df=15)+bmi.data$Gender)
- for(tau in 1:3/4){
-     fit <- rq(Glu ~ bs(Age, df=15)+bs(Total_chol,df=15)+Gender , tau=tau, data=bmi.data)
-     accel.fit <- X %*% fit$coef
-     plot(bmi.data$Age,accel.fit)
-     }
-
- ###########
- plot(times,accel,xlab = "milliseconds", ylab = "acceleration",type="n")
- points(times,accel,cex = .75)
- age_bs <- bs(times,intercept=FALSE,df=15)
- # generate a sequence of ages
- ages <- seq(min(bmi.data$Age),max(bmi.data$Age),by=1)
- # construct the b-spline expansion on the generated ages
- ages_bs <- bs(ages, degree = attr(age_bs, "degree"), knots = attr(age_bs, "knots"), Boundary.knots = attr(age_bs, "Boundary.knots"), intercept = FALSE)
- 
- for(i in c(6,9,15,22)){
-     # evaluate the spline basis times the coefficients
-     effect <- fit3[9,1,i]*ages_bs[, 1] + fit3[10,1,i] * ages_bs[, 2] + fit3[11,1,i] * ages_bs[, 3] + fit3[12,1,i] * ages_bs[, 4] + fit3[13,1,i] * ages_bs[, 5]
-     plot(ages,effect,type="n",xlab="Age",ylab="Age effect")
-     lines(ages,effect)
-     title(paste("Age effects on glucose", format(round(taus[i],2)),"Quantile"))
- }
- 
- 
- 
-  
- library(splines)
-  library(MASS)
-  data(mcycle)
-  attach(mcycle)
-  plot(bmi.data$Total_chol,bmi.data$Glu,xlab = "milliseconds", ylab = "acceleration",type="n")
-  points(bmi.data$Total_chol,bmi.data$Glu,cex = .75)
-  X <-model.matrix(bmi.data$Glu ~ bs(bmi.data$Total_chol, df=3))
-  for(tau in 1:1/4){
-      fit <- rq(Glu ~ bs(Total_chol, df=3), tau=tau, data=bmi.data)
-      accel.fit <- X %*% fit$coef
-      #accel.fit<- predict(fit,newdata =X,interval = "confidence")
-      lines(bmi.data$Total_chol,accel.fit[,1])
-  }
-  
-  attach(mcycle)
-  plot(bmi.data$Waist_cir,bmi.data$Glu,xlab = "milliseconds", ylab = "acceleration",type="n")
-  points(bmi.data$Waist_cir,bmi.data$Glu,cex = .75)
-  X <-model.matrix(bmi.data$Glu ~ bs(bmi.data$Total_chol, df=3))
-  for(tau in 1:3/4){
-      fit <- rq(Glu ~ bs(Waist_cir, df=3), tau=tau, data=bmi.data)
-      accel.fit <- X %*% fit$coef
-      #accel.fit<- predict(fit,newdata =X,interval = "confidence")
-      lines(bmi.data$Total_chol,accel.fit[,1])
-  }
-###########
-  #############
-  ##############
-  png(file = here::here("images", "newfig11.png"),
-      res = 400, height = 9, width = 16, units = "in")
-  par(mfrow=c(7,4))
-  quantreg.all <- rq(formula, tau = seq(0.05, 0.95, by = 0.05), data=bmi.data)
-  
-  quantreg.plot <- summary(quantreg.all)
-  
-  plot(quantreg.plot)
-  dev.off()
- 
-  
--- ci_splin.R
+++ ci_splin.R
@@ -1,38 +1,76 @@
-
+#Marginal effect on BMI with splines
+#Plot for the confidence interval of BMI regressed on splines of Age, 
+#race, gender,and cholesterol medications use.
  
 library(quantreg)
 library(splines)
+library(tidyverse)
 
-
+#memory.limit(size=56000)
 #attach(bmi.data)
-bmi.data=readRDS(file = "bmi.data.rds")
+dat=readRDS(file = "bmi.data.rds")
+#dat=dat[dat$BMI<28,]
+dat=filter(dat,Statin_status!='9')
+levels(dat$Statin_status) <- c('1','2')
+dat$Statin_status <- factor(dat$Statin_status)
+X <- model.matrix( BMI ~ bs(Total_chol, df = 5) + Race + Gender + Statin_status, data = dat)
+qr_fit_dat <- rq(  dat$BMI ~ X - 1, data=dat, tau = 0.9)
 
-formula <- Glu ~Gender+Race+BMI+Statin_status+bs(Age,intercept=FALSE,df=5)+bs(Total_chol,intercept=FALSE,df=5)+bs(waist_cir,intercept=FALSE,df=5)      
+ 
+#formula <- Glu ~Gender+Race+BMI+Statin_status+bs(Age,intercept=FALSE,df=5)+bs(Total_chol,intercept=FALSE,df=5)+bs(waist_cir,intercept=FALSE,df=5)
+# formula <- Glu ~ Gender+Race+Statin_status+bs(Age,intercept=FALSE,df=5)#+bs(Total_chol,intercept=FALSE,df=5)+bs(waist_cir,intercept=FALSE,df=5)      
+
+  
+ 
  
 #model <- rq(Glu ~ bs(Age, knots=c(25,50,75))+BMI+Gender, tau=0.5, data=bmi.data ) 
 
-model <- rq(formula, tau=0.5, data=bmi.data ) 
+# qr_fit_dat <- rq(formula, tau=0.5, data=bmi.data ) 
  
  
 #plot
+#age_pred <- seq(16, 80, by = 1)
+x2_pred <- factor(1:5)
+x3_pred <- factor(1:2)
+x4_pred <- factor(1:2)
+x5_pred <-seq(min(dat$Total_chol),400,by=1)
+dat_pred <- data.frame(expand.grid(age_pred, x2_pred, x3_pred, x4_pred))
+names(dat_pred) <- c( "Race", "Gender", "Statin_status","Total_chol" )
+
+dat_pred$Gender=recode(dat_pred$Gender, "1" = "Male", "2" = "Femal" )
+# RIDRETH1 (race, 1=mexican, 2=otherHispanic, 3= Non-Hispanic White,4=Non-Hispanic Black ,5=Other Race - Including Multi-Racial
+
+dat_pred$Race=recode(dat_pred$Race, "1" = "Mexican", "2" = "Other_Hispanic","3"="White","4"="Black","5"="Other" )
+# Classifying population to diabetes and non diabetes
+
+
+#colnames(dat) <- c("Age","Race","Gender","Statin_status","BMI")
+# X_pred <- model.matrix(~ (bs(x, df = 4) + Race + Gender) * Statin_status, data = dat_pred)
+X_pred <- model.matrix( ~ bs(Total_chol, df = 5) + Race + Gender+ Statin_status , data = dat_pred)
+
+
+
+dat_prediction <- list(X = X_pred)
+pred <- predict(qr_fit_dat, newdata =dat_prediction, interval = "confidence")
+dat_pred$pred  <- pred[, 1]
+dat_pred$lower  <- pred[, 2]
+dat_pred$higher <- pred[, 3]
+
 # Get model-based predictions
-pred <- as.data.frame(predict(model, data.frame(Age = bmi.data$Age, 
-    BMI = bmi.data$BMI, Gender = bmi.data$Gender,Race=bmi.data$Race, Statin_status=bmi.data$Statin_status, Total_chol=bmi.data$Total_chol,waist_cir=bmi.data$waist_cir),
-                              interval = "confidence"));
-pred$Age <- bmi.data$Age;
-
-#plot
-sp <- c(25,50,75);
-ggplot(bmi.data, aes(x=bmi.data$Age,y=bmi.data$Glu)) +
-    #geom_point() +
-    geom_line(data = pred, aes(x = Age, y = fit)) +
-    geom_ribbon(data = pred, aes(ymin = lower, ymax = higher, x = Age), alpha = 0.4) +
-    ylim(70,400)+
-    geom_quantile(formula = bmi.data$Glu ~ bs(bmi.data$Age,intercept=FALSE,df=5), quantiles = 0.5, se = T);
-
-
-
-
+ 
+    
+###############################################################################4
+fig2=dat_pred %>%
+    ggplot(aes(x = Total_chol, y = pred, color = Statin_status)) +
+    #geom_line() +
+    geom_quantile(formula = y ~ bs(x,intercept=FALSE,df=5), quantiles = 0.9)+
+    geom_ribbon(aes(x = Total_chol, ymin = lower, ymax = higher, fill = Statin_status), alpha = 0.1) +
+    scale_color_viridis_d(end = 0.7) +
+    ylim(25,50)+
+    scale_fill_viridis_d(end = 0.7)#+
+    #facet_grid(Race~Statin_status)
+    
+fig2
 
 
 
@@ -69,6 +107,6 @@ pred$x <- mydata$x;
 sp <- c(25,50,75);
 ggplot(mydata, aes(x=x,y=y)) +
     geom_point() +
-    geom_line(data = pred, aes(x = x, y = fit)) +
+     #geom_line(data = pred, aes(x = x, y = fit)) +
     geom_ribbon(data = pred, aes(ymin = lower, ymax = higher, x = x), alpha = 0.4) +
-    geom_quantile(formula = y ~ ns(x, knots = sp), quantiles = 0.5, se = T);
+    geom_quantile(formula = y ~ ns(x, knots = sp), quantiles = 0.8);
